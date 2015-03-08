b7368b89a5074ffad9f4df14b3ca105adffd3408
Arafat Mansur <amansur@gmail.com>
2015-03-08 00:58:26 -0500

    margin fixes

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
 data <- read.table("household_power_consumption.txt", header = T, sep=";", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), na.strings="?")
 data <- transform(data, Date = as.Date(Date, "%d/%m/%Y"))
 data <- subset(subset(data, Date >= as.Date("2007-02-01")), Date <= as.Date("2007-02-02"))
+par(oma=c("0,2,0,0"))
 hist(data$Global_active_power, col="red", main="Global Active Power", xlab ="Global Active Power (kilowatts)", mar = c(5.1,5.1,4.1,2.1))
 dev.copy(png, "plot1.png")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,6 +1,7 @@
 data <- read.table("household_power_consumption.txt", header = T, sep=";", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), na.strings="?")
 data <- transform(data, DateTime = as.POSIXct(paste(Date,Time), f= "%d/%m/%Y %H:%M:%S"))
 data <- subset(subset(data, DateTime >= as.POSIXct("2007-02-01 00:00:00", f="%Y-%m-%d %H:%M:%S")), DateTime <= as.POSIXct("2007-02-02 23:59:00", f="%Y-%m-%d %H:%M:%S"))
+par(oma=c("0,2,0,0"))
 with(data, plot(DateTime, Global_active_power, type="l", ylab = "Global Active Power (kilowatt)", xlab="", mar = c(5.1,5.1,4.1,2.1)))
 dev.copy(png, "plot2.png")
 dev.off()
