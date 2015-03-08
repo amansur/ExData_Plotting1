61854e12d37103e5079b2377c1555031374711bb
amansur <amansur@gmail.com>
2015-03-08 00:04:07 -0500

    Plot1 and Plot2

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", header = T, sep=";", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"), na.strings="?")
+data <- transform(data, Date = as.Date(Date, "%d/%m/%Y"))
+data <- subset(subset(data, Date >= as.Date("2007-02-01")), Date <= as.Date("2007-02-02"))
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab ="Global Active Power (kilowatts)", mar = c(5.1,5.1,4.1,2.1))
+dev.copy(png, "plot1.png")
+dev.off()
